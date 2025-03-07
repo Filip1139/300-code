6cdd7eb5ab5aedd92c0832c207fada282eebea12
filip1139 <filip1139@gmail.com>
2025-03-07 07:52:12 +0100

    feat: cn helper; svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
+import viteSvgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), viteSvgLoader()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
